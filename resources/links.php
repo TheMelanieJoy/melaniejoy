b8a4db2cf7359bcd3fae120d869f3c6b7025acab
TheMelanieJoy <m.damilig@hotmail.com>
2017-08-28 22:46:14 -0400

    Revert "v1.0"
    This reverts commit 38d0159cb689c0e0ae31d8ecab47172d805026b4.

--- resources/links.php
+++
@@ -1,8 +0,0 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>TheMelanieJoy</title>
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="css/main.css">
-<!-- Optional Bootstrap theme -->
-<link rel="stylesheet" href="css/bootstrap-reboot.min.css">
