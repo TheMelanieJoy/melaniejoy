38d0159cb689c0e0ae31d8ecab47172d805026b4
TheMelanieJoy <m.damilig@hotmail.com>
2017-08-28 22:45:55 -0400

    v1.0

---
+++ about/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include('../resources/links.php') ?>
+</head>
+<body>
+    <?php include('../resources/header.php') ?>
+
+    <div class="subbackground">
+        <img class="d-block ml-auto mr-auto" src="../resources/images/DSC_0070.jpg" alt="About Me">
+    </div>
+
+    <div class="bodycontainer">
+    <div class="aboutme">
+        <div class="jumbotron">
+            <h1>About Me</h1>
+            <p>Hi, I'm Melanie Joy. Passionate about video games for as long as I can remember, I decided to pursue a career in the field of computer science. I graduated from Vanier College in Computer Science Technology, a three-year technical program, but, yearning to learn more, I decided to continue my studies at Concordia University, majoring in Computer Games. That's right, I am one step closer to working in a field that I have admired since childhood!</p>
+            <br>
+            <p>Outside of computer science, I have other hobbies including music, digital art, photography, and video games. Playing the guitar and drawing have been some of the best ways for me to pass time and uplift my mood and have thus become passions. And so, I founded this website, whose purpose is to present both my web development and artistic abilities.</p>
+        </div>
+    </div>
+    </div>
+
+     <?php include('../resources/footer.php') ?>
+</body>
