3ec56c3fd694f13b84f6bd67f3bf55e3e7828b45
TheMelanieJoy <m.damilig@hotmail.com>
2017-08-28 22:46:57 -0400

    Revert "Revert "v1.0""
    This reverts commit b8a4db2cf7359bcd3fae120d869f3c6b7025acab.

---
+++ resources/footer.php
@@ -0,0 +1,24 @@
+<div class="footer">
+    <a href="https://melanaynay.tumblr.com/">
+        <span class="fa-stack fa-lg">
+            <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
+            <i class="fa fa-tumblr fa-stack-1x"></i>
+        </span>
+    </a>
+    <a href="http://themelaniejoy.deviantart.com/">
+        <span class="fa-stack fa-lg">
+            <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
+            <i class="fa fa-deviantart fa-stack-1x"></i>
+        </span>
+    </a>
+    <a href="https://www.linkedin.com/in/melanie-damilig-672095121/">
+        <span class="fa-stack fa-lg">
+            <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
+            <i class="fa fa-linkedin fa-stack-1x"></i>
+        </span>
+    </a>
+    <br>
+    <span>Copyright &copy TheMelanieJoy 2017</span>
+</div>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
